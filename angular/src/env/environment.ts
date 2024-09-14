a582511424a96576a0300d68b1bc81a2add2ca5a
Ashwin Sai Vagu <vagua01@pfw.edu>
2024-09-14 12:57:47 -0400

    changes in readme and apikeys

--- angular/src/env/environment.ts
+++ angular/src/env/environment.ts
@@ -1,12 +1,1 @@
-export const environment = {
-    production: false,
-    firebaseAuth : {
-    apiKey: "AIzaSyB2RMKYwBcxm2iXaQFwsjLngqbBU0UM89s",
-    authDomain: "cms-concept.firebaseapp.com",
-    projectId: "cms-concept",
-    storageBucket: "cms-concept.appspot.com",
-    messagingSenderId: "345065864296",
-    appId: "1:345065864296:web:1b7f0c8bd3703be02e8fdd",
-    measurementId: "G-2Z0S1KD9L4"
-    },
-  };
+export const environment = {};
